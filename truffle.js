f3d9e913c95a1fb9b0f17db63f283edf6486942b
ssteiger <soren1@gmx.net>
2019-06-23 10:25:15 +0200

    Set specific sol compiler version in truffle.js

--- truffle.js
+++ truffle.js
@@ -6,8 +6,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // match any network id
-      from: '0x954e72fdc51Cf919203067406fB337Ed4bDC8CdA', // account address from which to deploy
+      from: "0x954e72fdc51Cf919203067406fB337Ed4bDC8CdA", // account address from which to deploy
       gas: 4000000,
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24"
     }
   }
 };
